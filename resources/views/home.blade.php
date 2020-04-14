4fb9da6f372f15e14ef8021990288fdaa9e702ae
apisaraT <apisara_tungpennung@cmu.ac.th>
2020-04-15 03:22:40 +0700

    edit login

---
+++ resources/views/home.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>"WELCOME</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@500&display=swap" rel="stylesheet">
+  <style>
+    /* ________________________________LOGIN_______________________________________ */
+    .card {
+      width: 93%;
+      border-radius: 8px;
+      border: none;
+      box-shadow: 3px 3px 3px #A0A0A0;
+      padding:5%;
+      margin:3%;
+      display: grid;
+      grid-template-columns: auto auto;
+    }
+    body{
+        background: linear-gradient(to bottom, #01c6b2 0%, #fff 100%);
+        font-family: 'Mitr', sans-serif;
+    }
+    .hospital{
+        width:55%;
+        height:55%;
+        margin-left:5%;
+
+    }
+    .logo{
+        width:120px;
+        height:120px;
+    }
+    h1{
+        font-size:2vw;
+    }
+    .login{
+        margin-top:15%;
+        border-radius:4px;
+        background:#F6F6F6;
+        text-align:center;
+        width:150px;
+    }
+    
+  </style>
+</head>
+
+<body>
+    <div class="card">
+      <div class="left">
+        <img class="hospital" src="images/lo.png">
+      </div>
+      <div class="right">
+        <img class="logo" src="images/unknown.png">
+        <div class="login"> 
+            <h1>LOGIN</h1>
+        </div>
+        <div>
+        </div>
+      </div>
+      
+    </div>
+  
+
+</body>
+
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <style>
+        html,body{
+            background-color: #14CAB7;
+            overflow-y:hidden;
+        }
+    </style>
+
+    <title>NONG-SUKJAI</title>
+</head>
+<body>
+    <div class="container-fluid ">
+        <div id = "nav">
+            <div class="row" style="margin-top:1%">
+                <div class="col-3">
+                    <div class="row" style="margin-left:10px; margin-top:5%">
+                        <div class="col align-middle ">
+                            <button type="button" class="btn btn-warning" style="Width : 120px">ทั่วไป</button>
+                        </div>
+                        <div class="col align-middle">
+
+                            <button type="button" class="btn btn-warning" style="Width : 120px">เฉพาะหมอ</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-7">
+                    <div class="row">
+                        <div class="col-1">
+                            <img src="/images/logo.png" style="width : 80px;"/>
+                        </div>
+                        <div class="col" >
+                            <h1 class="display-4" style="color: #FFFFFF; margin-left: 20px">NONG SUKJAI</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="row">
+                        <div class="col">
+                            <p class="text" style="margin-top:20px">น้องเกมส์ เด็กดี</p>
+                        </div>
+                        <div class="col">
+                            <button type="button" class="btn btn-danger btn-sm" style="margin-top:20px">Logout</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row" style="margin-top:1%">
+            <div class="col-3 overflow-auto">
+                <div class="card">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card" style="margin-top:5%">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card" style="margin-top:5%">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card" style="margin-top:5%">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card" style="margin-top:5%">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card" style="margin-top:5%">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card" style="margin-top:5%">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card" style="margin-top:5%">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card" style="margin-top:5%">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+                <div class="card" style="margin-top:5%">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="card mh-100">
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
